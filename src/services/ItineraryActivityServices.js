28310aeb1f76de4865e0da830b2c76dcee607bda
Your Github username <d.pope@eagles.oc.edu>
2023-06-04 13:18:16 -0500

    Added flights, updated fields

--- src/services/ItineraryActivityServices.js
+++ src/services/ItineraryActivityServices.js
@@ -2,24 +2,39 @@ import apiClient from "./services";
 
 export default {
   getItineraryActivities() {
-    return apiClient.get("itineraryActivities");
+    return apiClient.get("itineraryActivities")
+      .catch(error => {
+        console.error('There was an error!', error.message);
+      });
   },
-  getItineraryActivitiesForItinerary(itineraryId) {
-    return apiClient.get("itineraries/" + itineraryId + "/itineraryActivities");
-  },
+
   getItineraryActivity(itineraryActivity) {
     return apiClient.get(
       "itineraries/" +
         itineraryActivity.itineraryId +
         "/itineraryActivities/" +
         itineraryActivity.id
-    );
+    )
+    .catch(error => {
+      console.error('There was an error!', error.message);
+    });
   },
+
+getItineraryActivitiesForItinerary(itineraryId) {
+    return apiClient.get("itineraries/" + itineraryId + "/itineraryActivities")
+      .catch(error => {
+        console.error('There was an error!', error.message);
+      });
+  },
+
   addItineraryActivity(itineraryActivity) {
     return apiClient.post(
       "itineraries/" + itineraryActivity.itineraryId + "/itineraryActivities",
       itineraryActivity
-    );
+    )
+    .catch(error => {
+      console.error('There was an error!', error.message);
+    });
   },
   updateItineraryActivity(itineraryActivity) {
     return apiClient.put(
@@ -28,7 +43,10 @@ export default {
         "/itineraryActivities/" +
         itineraryActivity.id,
       itineraryActivity
-    );
+    )
+    .catch(error => {
+      console.error('There was an error!', error.message);
+    });
   },
   deleteItineraryActivity(itineraryActivity) {
     return apiClient.delete(
@@ -36,6 +54,10 @@ export default {
         itineraryActivity.itineraryId +
         "/itineraryActivities/" +
         itineraryActivity.id
-    );
+    )
+    .catch(error => {
+      console.error('There was an error!', error.message);
+    });
   },
 };
+
--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,26 +1,38 @@
 import apiClient from "./services";
 
+async function getUser() {
+  return apiClient.get("users");
+}
+
+async function addUser(user) {
+  return apiClient.post("users", user);
+}
+
+async function loginUser(user) {
+  console.log(user);
+  return apiClient.post("login", user.value, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+      crossDomain: true,
+      Authorization: "Basic " + btoa(user.value.email + ":" + user.value.password),
+    },
+  });
+}
+
+async function logoutUser() {
+  return apiClient.post("logout");
+}
+
+async function forgotPassword(email) {
+  return apiClient.post("forgot-password", { email });
+}
+
 export default {
-  getUser() {
-    return apiClient.get("users");
-  },
-  addUser(user) {
-    return apiClient.post("users", user);
-  },
-  loginUser(user) {
-    console.log(user);
-    return apiClient.post("login", user.value, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        crossDomain: true,
-        Authorization:
-          "Basic " + btoa(user.value.email + ":" + user.value.password),
-      },
-    });
-  },
-  logoutUser() {
-    return apiClient.post("logout");
-  },
+  getUser,
+  addUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
 };
